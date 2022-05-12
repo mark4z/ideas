a89fcab891fefed46ff5ab9f8ea704a18a1d0240
mengchao.lv <anymre@outlook.com>
2022-05-12 14:32:21 +0800

    https

--- simple-api-benchmark/main.go
+++ simple-api-benchmark/main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"strconv"
 	"strings"
-	"sync"
-	"sync/atomic"
 )
 
 var ada = "123456"
@@ -17,138 +14,53 @@ var orderNumber = 123456789
 var cnt = 10
 var goroutineCnt = 20
 
-var gateway = "http://tw-openapi-pt.intranet.local/pt/user-center-privilege/eWallet/v1"
+var gateway = "http://3/pt/"
 var client = http.DefaultClient
-var token = "eyJraWQiOiIyQXFzRWdoWDEzSmxtQVNjaWc5a0loZFN5OW9abk9SSzE0MG9scFFvYWcwPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIzaGI3Mzd0ZG5sbDJyOG5kczA4aW5oMGhyciIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiZWNvbVwvYXBpLmFsbCIsImF1dGhfdGltZSI6MTY0OTY2NjYxMiwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmFwLW5vcnRoZWFzdC0xLmFtYXpvbmF3cy5jb21cL2FwLW5vcnRoZWFzdC0xX05aWDJUcXNHZCIsImV4cCI6MTY0OTc1MzAxMiwiaWF0IjoxNjQ5NjY2NjEyLCJ2ZXJzaW9uIjoyLCJqdGkiOiJlOTQwOTQyMi0zZjZhLTRiMjktYTczOC0xYWM0ODhiOGNlYTUiLCJjbGllbnRfaWQiOiIzaGI3Mzd0ZG5sbDJyOG5kczA4aW5oMGhyciJ9.C94ciSa4bYAhm6MSv_uAT0Tlz01HZ2htvVOYVYntf3LiveGU-49jNzOq3-35ZY2IwyocO8GR1NQx5ilxnBgdOIpu8XYgQ3Ih5EeMJ4ydlvQdy2H8WMxg4iYMmhjFW05nGD4zEhwe6oGaRqvCXjeMtcQgrPB9lqEVNXQayiYc5-7WETeY4arj_H3yqpYzyfCdV83l4OfYCTO2um7AZTGD8aYGDWlP7ImRuRvUe_jOVZbX8tXl-XjnociYqtAz8oArGkSz-RJ0b9vlr5MFqgFmQQ45tHJk5Du_kCIoOw3gBxMcOxWD1qmpphsbOc1tgWMnBjjVi_XqYGoYfZELZetrCw"
+var token = "1"
 
 func main() {
-	group := &sync.WaitGroup{}
-
-	group.Add(8)
-	go add(group, orderNumber)
-	go rec(group, orderNumber)
-
-	go consume(group, orderNumber)
-	go resume(group, orderNumber)
-
-	go add(group, orderNumber)
-	go rec(group, orderNumber)
-
-	go consume(group, orderNumber)
-	go resume(group, orderNumber)
-
-	group.Wait()
+	add(orderNumber)
 }
 
-func rec(w *sync.WaitGroup, number int) {
-	defer w.Done()
+func add(number int) {
+	no := 0
 
-	no := int64(number)
-	template := "{\"regionCode\":\"130\",\"bizCode\":\"INT\",\"language\":\"zh-TW\",\"channel\":\"INT\",\"transactionType\":\"COR\",\"aboNumber\":\"#{ada}\",\"referenceOrderNumber\":\"#{on}\"}"
+	template := "{\n  \"regionCode\": \"130\",\n  \"bizCode\": \"INT\",\n  \"language\": \"zh-TW\",\n  \"channel\": \"INT\",\n  \"items\": [\n    {\n      \"cityCode\": \"#{c}\",\n      \"districtCode\": \"#{d}\",\n      \"ada\": \"#{ada}\",\n      \"action\": 1,\n      \"memberFlag\": \"1\",\n      \"channel\": \"test\",\n      \"regionCode\": \"130\"\n    }\n  ]\n}"
 
-	waitGroup := sync.WaitGroup{}
-	for i := 0; i < cnt; i++ {
-		for j := 0; j < goroutineCnt; j++ {
-			waitGroup.Add(1)
-			go func() {
-				body := strings.Replace(strings.Replace(template, "#{ada}", ada, -1), "#{on}", strconv.Itoa(int(atomic.AddInt64(&no, 1))), -1)
-				for {
-					err := doRequest("recoverAccountBalance", body)
-					if err == nil {
-						break
-					}
-					log.Print("rec fail ", err)
-				}
-				waitGroup.Done()
-			}()
-		}
+	//open a file and read the content
+	file, err := ioutil.ReadFile("benchmark.txt")
+	if err != nil {
+		panic(err)
 	}
-	waitGroup.Wait()
-}
+	//split the content by \n
+	lines := strings.Split(string(file), "\n")
+	//create a goroutine for each line
+	for _, line := range lines {
+		split := strings.Split(line, "\t")
 
-func add(w *sync.WaitGroup, number int) {
-	defer w.Done()
-
-	no := int64(number)
-	template := "{\"regionCode\":\"130\",\"bizCode\":\"INT\",\"language\":\"zh-TW\",\"channel\":\"INT\",\"accounts\":[{\"type\":1,\"amount\":100.01},{\"type\":2,\"amount\":100.02}],\"transactionType\":\"GOR\",\"aboNumber\":\"#{ada}\",\"referenceOrderNumber\":\"#{on}\"}"
-
-	waitGroup := sync.WaitGroup{}
-	for i := 0; i < cnt*2; i++ {
-		for j := 0; j < goroutineCnt; j++ {
-			waitGroup.Add(1)
-			go func() {
-				body := strings.Replace(strings.Replace(template, "#{ada}", ada, -1), "#{on}", strconv.Itoa(int(atomic.AddInt64(&no, 1))), -1)
-				for {
-					err := doRequest("addAccountBalance", body)
-					if err == nil {
-						break
-					}
-					log.Print("add fail ", err)
-				}
-				waitGroup.Done()
-			}()
+		i := strings.Split(split[0], "_")
+		//split the line by ,
+		body := strings.Replace(template, "#{ada}", split[1], -1)
+		body = strings.Replace(body, "#{c}", i[0], -1)
+		if len(i) > 1 {
+			body = strings.Replace(body, "#{d}", i[1], -1)
+		} else {
+			body = strings.Replace(body, "#{d}", "", -1)
 		}
-	}
-	waitGroup.Wait()
-}
-
-func resume(w *sync.WaitGroup, number int) {
-	defer w.Done()
-
-	no := int64(number)
-	template := "{\"regionCode\":\"130\",\"bizCode\":\"INT\",\"language\":\"zh-TW\",\"channel\":\"INT\",\"transactionType\":\"CCR\",\"aboNumber\":\"#{ada}\",\"referenceOrderNumber\":\"#{on}\"}"
-
-	waitGroup := sync.WaitGroup{}
-	for i := 0; i < cnt; i++ {
-		for j := 0; j < goroutineCnt; j++ {
-			waitGroup.Add(1)
-			go func() {
-				body := strings.Replace(strings.Replace(template, "#{ada}", ada, -1), "#{on}", strconv.Itoa(int(atomic.AddInt64(&no, 1))), -1)
-				for {
-					err := doRequest("resumeAccountBalance", body)
-					if err == nil {
-						break
-					}
-					log.Print("resume fail ", err)
-				}
-				waitGroup.Done()
-			}()
+		err = doRequest("", body)
+		if err != nil {
+			fmt.Println(err)
 		}
+		fmt.Println(no)
+		no++
 	}
-	waitGroup.Wait()
-}
-
-func consume(w *sync.WaitGroup, number int) {
-	defer w.Done()
-
-	no := int64(number)
-	template := "{\"regionCode\":\"130\",\"bizCode\":\"INT\",\"language\":\"zh-TW\",\"channel\":\"INT\",\"accounts\":[{\"type\":1,\"amount\":100.01},{\"type\":2,\"amount\":100.02}],\"transactionType\":\"UCR\",\"aboNumber\":\"#{ada}\",\"referenceOrderNumber\":\"#{on}\"}"
-
-	waitGroup := sync.WaitGroup{}
-	for i := 0; i < cnt; i++ {
-		for j := 0; j < goroutineCnt; j++ {
-			waitGroup.Add(1)
-			go func() {
-				body := strings.Replace(strings.Replace(template, "#{ada}", ada, -1), "#{on}", strconv.Itoa(int(atomic.AddInt64(&no, 1))), -1)
-				for {
-					err := doRequest("consumeAccountBalance", body)
-					if err == nil {
-						break
-					}
-					log.Print("consume fail ", err)
-				}
-				waitGroup.Done()
-			}()
-		}
-	}
-	waitGroup.Wait()
 }
 
 func doRequest(url, requestBody string) error {
 	request, _ := http.NewRequest("POST", gateway+"/"+url, strings.NewReader(requestBody))
 	request.Header.Add("Content-Type", "application/json")
-	request.Header.Add("x-api-key", "E8K7PMSAR217STANCBK2")
-	request.Header.Add("x-apigw-api-id", "3a8r77t89b")
+	request.Header.Add("x-api-key", "1")
+	request.Header.Add("x-apigw-api-id", "2")
 	request.Header.Add("Authorization", "Bearer "+token)
 	do, err := client.Do(request)
 	if err != nil {
---
+++ websocket/go.mod
@@ -0,0 +1,12 @@
+module websocket
+
+go 1.17
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/fasthttp/websocket v1.5.0 // indirect
+	github.com/klauspost/compress v1.14.1 // indirect
+	github.com/savsgio/gotils v0.0.0-20211223103454-d0aaa54c5899 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.33.0 // indirect
+)
