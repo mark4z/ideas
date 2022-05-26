68abcec6205e8846b6fdcce586d64e348270fa69
mengchao.lv <anymre@outlook.com>
2022-05-26 15:22:32 +0800

    done

--- docker/main.go
+++ docker/main.go
@@ -1,19 +1,47 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path"
 	"syscall"
 )
 
+const (
+	cgroupMemoryHierarchyMount = "/sys/fs/cgroup/memory"
+)
+
 func main() {
-	cmd := exec.Command("sh")
-	cmd.SysProcAttr = &syscall.SysProcAttr{}
+	if os.Args[0] == "/proc/self/exe" {
+		fmt.Printf("current pid %d\n", syscall.Getpid())
+		fmt.Println()
+		cmd := exec.Command("sh", "-c", `stress --vm-bytes 200M --vm-keep -m 1`)
+		cmd.SysProcAttr = &syscall.SysProcAttr{}
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+	cmd := exec.Command("/proc/self/exe")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+		fmt.Println("-", err)
+		os.Exit(1)
+	} else {
+		fmt.Printf("%v\n", cmd.Process.Pid)
+		os.Mkdir(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit"), 0755)
+		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "tasks"), []byte(fmt.Sprintf("%d", cmd.Process.Pid)), 0644)
+		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "memory.limit_in_bytes"), []byte("100M"), 0644)
 	}
+	cmd.Process.Wait()
 }
